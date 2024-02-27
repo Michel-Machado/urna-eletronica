8b0ccb1bf6e2ff2d0f74a3dfda58f394c4e6e64b
Michel Machado <michelmachado125@gmail.com>
2024-02-27 13:15:04 -0300

    primeiro commit

---
+++ src/test/java/com/grupo04pj01/urna/UrnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo04pj01.urna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
