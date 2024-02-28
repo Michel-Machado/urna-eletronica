63a2133b3faf53912126286c0c5a2ba13c46015f
Michel Machado <michelmachado125@gmail.com>
2024-02-28 08:31:47 -0300

    URNA03- tabela eleitoresPresentes

---
+++ src/main/java/com/grupo04pj01/urna/models/EleitorPresenteModel.java
@@ -0,0 +1,23 @@
+package com.grupo04pj01.urna.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "eleitores_presente_tb")
+public class EleitorPresenteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "eleitor_id")
+    public EleitorModel eleitorModel;
+
+}
