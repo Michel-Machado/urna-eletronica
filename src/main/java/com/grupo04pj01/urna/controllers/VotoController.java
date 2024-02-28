a2bcadd88d0ffa40bb92571ee014305f07806d58
Michel Machado <michelmachado125@gmail.com>
2024-02-28 17:45:56 -0300

    URNA05- fluxo de votação

---
+++ src/main/java/com/grupo04pj01/urna/controllers/VotoController.java
@@ -0,0 +1,33 @@
+package com.grupo04pj01.urna.controllers;
+
+import com.grupo04pj01.urna.models.CandidatoModel;
+import com.grupo04pj01.urna.models.VotosModel;
+import com.grupo04pj01.urna.services.CandidatoService;
+import com.grupo04pj01.urna.services.VotoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/voto")
+public class VotoController {
+    private final VotoService votoService;
+
+    @PostMapping
+    public ResponseEntity<Void> votar(@RequestBody VotosModel votosModel){
+     votoService.votar(votosModel);
+     return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{id}")
+    public Integer buscaVotosPorCandidato(@PathVariable Long id){
+       Integer totalVotos= votoService.contarVotosById(id);
+
+        return totalVotos;
+    }
+
+}
---
+++ src/main/java/com/grupo04pj01/urna/repositories/VotoRepository.java
@@ -0,0 +1,16 @@
+package com.grupo04pj01.urna.repositories;
+
+import com.grupo04pj01.urna.models.EleitorModel;
+import com.grupo04pj01.urna.models.VotosModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VotoRepository extends JpaRepository<VotosModel, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT COUNT(*) FROM votos_tb WHERE candidato_id = :candidatoId")
+    Integer contarVotosByCandidatoId(Long candidatoId);
+
+
+}
