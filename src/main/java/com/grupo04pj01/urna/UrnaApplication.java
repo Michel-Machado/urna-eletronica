d1396552ff4e72aa0eaeb47406df3252398a42b9
Michel Machado <michelmachado125@gmail.com>
2024-03-16 10:41:59 -0300

    [URNA-17]-scaneamento-de-componentes-adicionado-para-corrigir-bug-na-geração-de-artefato

--- src/main/java/com/grupo04pj01/urna/UrnaApplication.java
+++ src/main/java/com/grupo04pj01/urna/UrnaApplication.java
@@ -3,8 +3,10 @@ package com.grupo04pj01.urna;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.grupo04pj01.urna"})
 public class UrnaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/grupo04pj01/urna/repositories/CandidatoRepository.java
+++ src/main/java/com/grupo04pj01/urna/repositories/CandidatoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
 @Repository
 public interface CandidatoRepository extends JpaRepository<CandidatoModel, Long> {
 
