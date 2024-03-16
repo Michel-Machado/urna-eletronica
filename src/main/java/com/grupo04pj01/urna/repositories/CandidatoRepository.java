c53a35fa303a26aced24847aef3525d59bb66114
Michel Machado <michelmachado125@gmail.com>
2024-03-15 22:24:10 -0300

    controller de presenca

--- src/main/java/com/grupo04pj01/urna/repositories/CandidatoRepository.java
+++ src/main/java/com/grupo04pj01/urna/repositories/CandidatoRepository.java
@@ -1,7 +1,6 @@
 package com.grupo04pj01.urna.repositories;
 
 import com.grupo04pj01.urna.models.CandidatoModel;
-import com.grupo04pj01.urna.models.EleitorModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/grupo04pj01/urna/repositories/VotoRepository.java
+++ src/main/java/com/grupo04pj01/urna/repositories/VotoRepository.java
@@ -1,6 +1,5 @@
 package com.grupo04pj01.urna.repositories;
 
-import com.grupo04pj01.urna.models.EleitorModel;
 import com.grupo04pj01.urna.models.VotosModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
